118bf12486e26af81570392b64757af58ed71211
RafaelaMouraa <rafaela.araujo.moura@gmail.com>
2024-05-10 12:22:50 -0300

    inclusao de tela de barbearia, clique em voltar, recomendações e favoritos

--- fullstackweek-barber/app/(home)/page.tsx
+++ fullstackweek-barber/app/(home)/page.tsx
@@ -1,10 +1,57 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
+import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
+import BarbershopItem from "./_components/barbershop-item";
+import { db } from "../_lib/prisma";
 
-export default function Home() {
-  return (
+
+export default async function Home() {
+  //chamar prisma e pegar barbearias
+  const barbershops = await db.barbershop.findMany({})
+    return (
     <div>
       <Header />
+
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xL font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR
+          })}</p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+        <BookingItem></BookingItem>
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 text-sm mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+        
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-6 mb[4.5rem]">
+        <h2 className="px-5 text-sm mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+        
+        <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
